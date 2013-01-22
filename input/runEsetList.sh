4454a74b5131c08155abf1881b1922f458398780
lima1 <markus@jimmy.harvard.edu>
2013-01-22 15:23:46 -0500

    changed definition debulking for TCGA back to no macroscopic disease

--- input/runEsetList.sh
+++ input/runEsetList.sh
@@ -7,4 +7,3 @@ R --vanilla "--args patientselection2validation.config.hold eset.validation.scal
 R --vanilla "--args patientselection2debulking.config.hold eset.debulking.scaled.rda eset.debulking.scaled.log"  < createEsetList.R
 R --vanilla "--args patientselection2debulkingnoscale.config.hold eset.debulking.rda eset.debulking.log"  < createEsetList.R
 R --vanilla "--args patientselection2allos.config.hold eset.allos.scaled.rda eset.allos.scaled.log"  < createEsetList.R
-R --vanilla "--args patientselection2inclgrade2.config.hold eset.inclgrade2.scaled.rda eset.inclgrade2.scaled.log"  < createEsetList.R
